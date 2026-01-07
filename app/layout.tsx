7f8d9581bbf56537fed84f93e133f3a31db3b32f
jacombita1 <136485947+jacombita1@users.noreply.github.com>
2026-01-07 10:32:23 -0500

    fix code metadata google witout meta label

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,7 +82,7 @@ export const metadata: Metadata = {
     maximumScale: 5,
   },
   verification: {
-    google: '<meta name="google-site-verification" content="-YVFBmqpYDeWZbFn7YPybBV8cwEtiEoFaadZhRMkUKM" />',
+    google: '-YVFBmqpYDeWZbFn7YPybBV8cwEtiEoFaadZhRMkUKM',
   },
   alternates: {
     canonical: 'https://jaime-combita-landing.vercel.app/',
